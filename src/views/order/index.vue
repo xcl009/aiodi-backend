7f53ca7da87b52bc63ec58b7e6cb4bbfe1929639
陈爽 <993379615@qq.com>
2023-11-18 09:53:35 +0000

    优化订单页面
    Signed-off-by: 陈爽 <993379615@qq.com>

--- src/views/order/index.vue
+++ src/views/order/index.vue
@@ -1230,6 +1230,7 @@ export default {
           } else if (dialogType == 2) {
             this.$set(this.dform, 'refundType', '0')
             if (row.amountEnable && row.amountEnable > 0) {
+              this.Constant.RefundType[3] = '余额退款';
               this.editObj.refundType = '3';
             }
 
